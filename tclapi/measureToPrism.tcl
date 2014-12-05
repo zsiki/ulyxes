113828438ff1c57bf1d2213e553b8770c235b91d
zsiki <siki@agt.bme.hu>
2014-12-05 20:45:11 +0100

    add LockIn call and coordinates to output

--- tclapi/measureToPrism.tcl
+++ tclapi/measureToPrism.tcl
@@ -49,31 +49,31 @@ if {[OpenCom $com]} {
 }
 if {$dist} {
 	SetEDMMode $mode	;# distance measurement mode
-	SetLock 0		;# turn off lock
-	SetATR 1		;# set automatic target recognition
+	SetLock 0			;# turn off lock
+	SetATR 1			;# set automatic target recognition
 	MoveRel 0 0 "RAD" 1	;# target on prism with ATR
 	set res [Measure]	;# initial measurement
 	set systemTime [clock seconds]  ;# set current time
 	if {[llength $res] > 2} {
+		set sd [GetVal 9 $res]	;# initial slope distance
 		puts "[DMS [GetVal 7 $res]] [DMS [GetVal 8 $res]] [GetVal 9 $res] [clock format $systemTime -format %H:%M:%S]"
-#		puts "[GetVal 7 $res] [GetVal 8 $res] [GetVal 9 $res] [clock format $systemTime -format %H:%M:%S]"
 	} else {
 		puts "*** $res [clock format $systemTime -format %H:%M:%S]"
 	}
 	if {$dist == 3 || $dist == 4} {
 		SetLock 1	;# lock on target
-		MoveRel 0 0 "RAD" 1	;# target on prism with ATR
+		LockIn
+#		MoveRel 0 0 "RAD" 1	;# target on prism with ATR
 	}
 } else {
 	SetATR 0
-	SetEDMMode 5
+	SetEDMMode $mode
 }
 
 while {1} {
 	set systemTime [clock seconds]  ;# set current time
 	switch -exact $dist {
 		0 {
-#			set res [GetAngles]	;# get angles only
 			set res [Measure]	;# distance measurement
 		}
 		1 {
@@ -85,16 +85,20 @@ while {1} {
 			set res [GetAngles]	;# get angles only
 		}
 		3 {
-			set res [GetAngles]	;# get angles only
+			set res [GetAngles]	;# get angles only during lock
 		}
 		4 {
-			set res [Measure]	;# distance measurement
+			set res [Measure]	;# distance measurement during lock
 		}
 	}
 	# write data to standard output
 	if {[llength $res] >= 2} {
-		puts "[DMS [GetVal 7 $res]] [DMS [GetVal 8 $res]] [GetVal 9 $res] [clock format $systemTime -format %H:%M:%S]"
-#		puts "[GetVal 7 $res] [GetVal 8 $res] [GetVal 9 $res] [clock format $systemTime -format %H:%M:%S]"
+		if {[GetVal 9 $res] == ""} {
+			set sd [GetVal 9 $red]
+		}
+		set dy [format "%10.4f" [expr {$sd * sin([GetVal 8 $res]) * sin([GetVal 7 $res])}]]
+		set dx [format "%10.4f" [expr {$sd * sin([GetVal 8 $res]) * cos([GetVal 7 $res])}]]
+		puts "$dy $dx [DMS [GetVal 7 $res]] [DMS [GetVal 8 $res]] [format "%10.4f" [GetVal 9 $res]] [clock format $systemTime -format %H:%M:%S]"
 	} else {
 		puts "*** $res [clock format $systemTime -format %H:%M:%S]"
 	}
