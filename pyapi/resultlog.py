d242501e9bbd4e6221765b75f040382da3390633
zsiki <siki@agt.bme.hu>
2015-01-21 13:50:20 +0100

    identation corrected

--- pyapi/resultlog.py
+++ pyapi/resultlog.py
@@ -21,11 +21,11 @@ class ResultLog(object):
         """ initialize log file if the given file cannot be opened for output then a log.log file in the temperary directory will be used
 
             :param logfile: name of the log file it will be created if neccessary, messages will be appended to the end
-			:param log_level: level of logs 0/1/2 error/warning/debug
+            :param log_level: level of logs 0/1/2 error/warning/debug
             :param repeat_count: retry count on fail accessing log file
         """
         self.repeat_count = repeat_count   # retry count for i/o operations
-		self.log_level = log_level         # level of messages
+        self.log_level = log_level         # level of messages
         for i in range(self.repeat_count * 2):
             try:
                 f = open(logfile, "a")
@@ -52,8 +52,8 @@ class ResultLog(object):
 
             :param msg: message to write
         """
-		if level > self.log_level:
-			return
+        if level > self.log_level:
+            return
 
         for i in range(self.repeat_count):
             try:
