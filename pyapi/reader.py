dbb5f3c7f0576ee20413986df37750111af40263
zsiki <siki@agt.bme.hu>
2015-02-06 00:55:12 +0100

    initial commit

---
+++ pyapi/reader.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. module:: reader.py
+   :platform: Unix, Windows
+   :synopsis: Ulyxes - an open source project to drive total stations and
+           publish observation results.
+           GPL v2.0 license
+           Copyright (C) 2010-2013 Zoltan Siki <siki@agt.bme.hu>
+
+.. moduleauthor:: Zoltan Siki <siki@agt.bme.hu>
+"""
+
+class Reader(object):
+    """ Base class for different readers (virtual)
+    """
+    RD_OK = 0
+    RD_OPEN = -1
+    RD_READ = -2
+
+    def __init__(self, name = 'None'):
+        """ Constructor
+
+            :param name: name of reader
+        """
+        self.name = name
+        self.state = self.RD_OK
+
+    def GetName(self):
+        """ Get the name of the interface being used.
+        """
+        return self.name
+
+    def GetState(self):
+        """ Get the state of the interface being used.
+        """
+        return self.state
+
+    def ClearState(self):
+        """ Clear the state of the reader being used.
+        """
+        self.state = self.RD_OK
