71f99dacd8750047e61e149babebfad5e403bc74
zsiki <siki@agt.bme.hu>
2015-03-27 23:01:55 +0100

    adding more comment

--- pyapps/horizsection.py
+++ pyapps/horizsection.py
@@ -5,9 +5,11 @@
 .. moduleauthor:: Viktoria Zubaly, Zoltan Siki
 
     Sample application of Ulyxes PyAPI to measure a horizontal section
+    target on the first point of the section and start this app
+    coordinates and observations are written to csv file
 
-	:param argv[1] (angle step): angle step between point in DEG, default 45
-	:param argv[2] (sensor): 1100/1800/1200, default 1200
+    :param argv[1] (angle step): angle step between points in DEG, default 45
+    :param argv[2] (sensor): 1100/1800/1200, default 1200
     :param argv[3] (port): serial port, default COM7
 """
 import sys
@@ -23,6 +25,7 @@ from csvwriter import CsvWriter
 from totalstation import TotalStation
 
 logging.getLogger().setLevel(logging.WARNING)
+# process commandline parameters
 if len(sys.argv) > 1:
     stepinterval = Angle(float(sys.argv[1]), 'DEG')
 else:
@@ -42,58 +45,59 @@ elif re.search('550[0-9]$', stationtype):
     mu = Trimble5500()
 else:
     print "unsupported instrument type"
-	exit(1)
+    exit(1)
 if len(sys.argv) > 3:
     port = sys.argv[3]
 else:
     port = 'COM7'
-MAXITER = 10
+MAXITER = 10    # number of iterations to find point on horizontal plan
 iface = SerialIface("rs-232", port)
 wrt = CsvWriter(angle = 'DMS', dist = '.3f', filt = ['id','hz','v','distance','east','north','elev'], fname = 'stdout', mode = 'a', sep = ';')
 ts = TotalStation(stationtype, mu, iface)
-ts.SetEDMMode('RLSTANDARD')
-ts.Measure()
+ts.SetEDMMode('RLSTANDARD') # reflectorless distance measurement
+ts.Measure()    # initial measurement for startpoint
 startp = ts.GetMeasure()
 if ts.measureIface.state != ts.measureIface.IF_OK or 'errorCode' in startp:
-    print 'Start again!'
+    print 'FATAL Cannot measure startpoint'
     exit(1)
 
-act = Angle(0)
+act = Angle(0)  # actual angle from startpoint
+# height of startpoint above the horizontal axis
 height0 = math.cos(startp['v'].GetAngle()) * startp['distance']
 w = True
-while act.GetAngle() < PI2:
-   ts.Measure()
-   if ts.measureIface.state != ts.measureIface.IF_OK:
-       ts.measureIface.state = ts.measureIface.IF_OK
-       ts.MoveRel(stepinterval, Angle(0))
-       continue
-   nextp = ts.GetMeasure()
-   if ts.measureIface.state != ts.measureIface.IF_OK:
-       ts.measureIface.state = ts.measureIface.IF_OK
-       ts.MoveRel(stepinterval, Angle(0))
-       continue
+while act.GetAngle() < PI2: # go around the whole circle
+    ts.Measure() # measure distance0
+    if ts.measureIface.state != ts.measureIface.IF_OK:
+        ts.measureIface.state = ts.measureIface.IF_OK
+        ts.MoveRel(stepinterval, Angle(0))
+        continue
+    nextp = ts.GetMeasure()  # get observation data
+    if ts.measureIface.state != ts.measureIface.IF_OK:
+        ts.measureIface.state = ts.measureIface.IF_OK
+        ts.MoveRel(stepinterval, Angle(0))
+        continue
 
-   height = math.cos(nextp['v'].GetAngle()) * nextp['distance']
-   index = 0
-   while abs(height-height0) > 0.01:
-       w = True
-       zenith = nextp['v'].GetAngle()
-       zenith1 = math.acos(height0 / nextp['distance'])	
-       ts.MoveRel(Angle(0), Angle(zenith1-zenith))	   
-       ts.Measure()
-       index += 1
-       if index > MAXITER or ts.measureIface.state != ts.measureIface.IF_OK:
-           w = False
-           ts.measureIface.state = ts.measureIface.IF_OK
-           logging.warning('Missing measurement')
-           break
-       nextp = ts.GetMeasure()
-       if not 'v' in nextp or not 'distance' in nextp:
-           break
-       height = math.cos(nextp['v'].GetAngle()) * nextp['distance']
-   if 'distance' in nextp and w:
-       coord = ts.Coords()
-       res = dict(nextp.items()+coord.items())
-       wrt.WriteData(res)
-   ts.MoveRel(stepinterval, Angle(0))
-   act += stepinterval
+    height = math.cos(nextp['v'].GetAngle()) * nextp['distance']
+    index = 0
+    while abs(height-height0) > 0.01:   # looking for right elevation
+        w = True
+        zenith = nextp['v'].GetAngle()
+        zenith1 = math.acos(height0 / nextp['distance'])
+        ts.MoveRel(Angle(0), Angle(zenith1-zenith))   
+        ts.Measure()
+        index += 1
+        if index > MAXITER or ts.measureIface.state != ts.measureIface.IF_OK:
+            w = False
+            ts.measureIface.state = ts.measureIface.IF_OK
+            logging.warning('Missing measurement')
+            break
+        nextp = ts.GetMeasure()
+        if not 'v' in nextp or not 'distance' in nextp:
+            break
+        height = math.cos(nextp['v'].GetAngle()) * nextp['distance']
+    if 'distance' in nextp and w:
+        coord = ts.Coords()
+        res = dict(nextp.items()+coord.items())
+        wrt.WriteData(res)
+    ts.MoveRel(stepinterval, Angle(0))
+    act += stepinterval
