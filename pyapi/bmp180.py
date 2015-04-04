45b4deeed14eb4e81b1b315d5561561e69341474
zsiki <siki@agt.bme.hu>
2015-04-04 13:30:21 +0200

    Altitude calculation added

--- pyapi/bmp180.py
+++ pyapi/bmp180.py
@@ -27,6 +27,7 @@ class BMP180(Instrument):
         """ constructor
         """
         super(BMP180, self).__init__(name, measureUnit, measureIface, writerUnit)
+        self.p0 = None    # sealevel pressure not set
         self.LoadCalibration()
 
     def LoadCalibration(self):
@@ -34,11 +35,14 @@ class BMP180(Instrument):
         #self.measureUnit.cal_AC1 = self.measureIface.Send(('readS16BE', BMP180_CAL_AC1))['data']
         pass
 
-    def GetPressure(self):
+    def GetPressure(self, withTemp = 1):
         """ Get pressure from sensor
 
+            :param withTemp: measure temperature also for fresh correction value (B5 sotred in measure unit)
             :returns: pressure in Pascals
         """
+        if withTemp:
+            self.GetTemp()
         msg = self.measureUnit.GetPressureMsg()
         return self._process(msg)
 
@@ -50,6 +54,26 @@ class BMP180(Instrument):
         msg = self.measureUnit.GetTempMsg()
         return self._process(msg)
 
+    def SetSealevel(self, altitude, pressure = None):
+        """ calculate sealevel pressure from known elevation and pressure
+
+            :param altitude: known elevation (float) meters
+            :param pressure: know pressure at elevation, default None means get the pressure from sensor
+        """
+        if pressure is None:
+            pressure = self.GetPressure()['pressure']
+        self.p0 = pressure / pow(1.0 - altitude / 44330.0, 5.255)
+
+    def GetAltitude(self):
+        """ calculate altitude from sealevel pressure
+
+            :returns: altitude
+        """
+        if self.p0 is None:
+            return None    # no reference pressure
+        pressure = self.GetPressure()['pressure']
+        return 44330.0 * (1.0 - pow(pressure / self.p0, (1.0 / 5.255)))
+
 if __name__ == "__main__":
     from bmp180measureunit import BMP180MeasureUnit
     from i2ciface import I2CIface
@@ -58,6 +82,7 @@ if __name__ == "__main__":
     i2c = I2CIface(None, 0x77)
     ew = EchoWriter()
     bmp = BMP180('BMP180', mu, i2c, ew)
-    #bmp = BMP180('BMP180', mu, i2c)
-    print bmp.GetTemp()
-    print bmp.GetPressure()
+    bmp.GetTemp()
+    bmp.GetPressure()
+    bmp.SetSealevel(105.0)
+    print bmp.GetAltitude()
