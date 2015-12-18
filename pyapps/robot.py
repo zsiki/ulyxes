ced2d3d905e4a128ac0201aa46b4fa394be125c1
zsiki <siki@agt.bme.hu>
2015-12-18 21:08:34 +0100

    code redesigned to oop

--- pyapps/robot.py
+++ pyapps/robot.py
@@ -4,7 +4,7 @@
 
 .. moduleauthor:: Zoltan Siki
 
-Sample application of Ulyxes PyAPI to measure a series of points
+Sample application of Ulyxes PyAPI to measure a serie of points
 
     :param argv[1] input file with directions
     :param argv[2] output file with observations
@@ -21,6 +21,7 @@ Sample geo::
     {5 T2} {7 3.006678} {8 1.550763} {4 ATR1} {112 2}
     {5 4} {7 3.145645} {8 1.610680} {4 PR2} {112 2}
     {5 1} {7 6.002123} {8 1.172376} {4 PR} {112 2}
+    {5 9} {7 6.235123} {8 1.178538} {4 RLA} {112 2}
 
 Sample dmp::
 
@@ -35,9 +36,10 @@ Sample dmp::
 Codes describe target type::
 
     ATRn - prism and automatic targeting, n referes to prism type 0/1/2/3/4/5/6/7 round/mini/tape/360/user1/user2/user3/360 mini
-    PRn - prism, n referes to prism type 0/1/2/3/4/5/6/7 round/mini/tape/360/user1/user2/user3/360 mini
-    RL - refrectorless observation
-    OR - do not measure distance (orientation)
+    PRn - prism, n referes to prism type 0/1/2/3/4/5/6/7 round/mini/tape/360/user1/user2/user3/360 mini, manual targeting
+    RL - refrectorless observation, manual targeting
+    RLA - reflectorless observation (automatic)
+    OR - do not measure distance (orientation), manual targeting
 
     In case of PR/RL/OR the program stops and the user have to aim at the target
 """
@@ -52,181 +54,229 @@ from angle import Angle, PI2
 from serialiface import SerialIface
 from csvwriter import CsvWriter
 from georeader import GeoReader
+from csvreader import CsvReader
 from httpwriter import HttpWriter
 from totalstation import TotalStation
 
 logging.getLogger().setLevel(logging.WARNING)
 
-if __name__ == "__main__":
-    # defaults
-    conf = False
-    ofname = 'stdout'
-    stationtype = '1100'
-    port = '/dev/ttyUSB0'
-    # process commandline parameters
-    if len(sys.argv) > 1:
-        ifname = sys.argv[1]
+class Robot(object):
+    """ manage robotic observations
+    """
+
+    def __init__(self, ifname, ofname = 'stdout', stationtype = '1100',
+        port = '/dev/ttyUSB0', maxtry = 3):
+        """ initialize
+        """
         if ifname[-3:] == '.py':  # configuration file given
-            conf = True
             exec 'from ' + ifname[:-3] + ' import *'
         elif ifname[-4:] != '.geo' and ifname[-4:] != '.dmp':
             ifname = ifname + '.geo'
+        if ofname[-4:] == '.dmp' or ofname[-4:] == '.csv' or ofname == 'stdout':
+            # file or console output
+            if ofname[-4:] == '.dmp' or ofname[-4:] == '.csv':
+                ofname1 = ofname[:-4] + '.dmp'
+                ofname2 = ofname[:-4] + '.csv'
+            else:
+                ofname1 = ofname2 = ofname
+            self.dmp_wrt = CsvWriter(angle = 'DMS', dist = '.4f', \
+                filt = ['station', 'id','hz','v','distance', 'datetime'], \
+                fname = ofname1, mode = 'a', sep = ';')
+            self.coo_wrt = CsvWriter(dist = '.4f', \
+                filt = ['id', 'east', 'north', 'elev', 'datetime'], \
+                fname = ofname2, mode = 'a', sep = ';')
+        elif ofname[:5] == 'http:' or ofname[:6] == 'https:':
+            # http output
+            ofname1 = ofname2 = ofname
+            self.dmp_wrt = HttpWriter(angle = 'RAD', dist = '.4f', \
+                filt = ['station', 'id','hz','v','distance', 'datetime'], \
+                url = ofname1, mode = 'POST')
+            self.coo_wrt = HttpWriter(angle = 'RAD', dist = '.4f', \
+                filt = ['id', 'east', 'north', 'elev', 'datetime'], \
+                url = ofname2, mode = 'POST')
+        if re.search('120[0-9]$', stationtype):
+            from leicatps1200 import LeicaTPS1200
+            mu = LeicaTPS1200()
+        elif re.search('110[0-9]$', stationtype):
+            from leicatcra1100 import LeicaTCRA1100
+            mu = LeicaTCRA1100()
+        elif re.search('180[0-9]$', stationtype):
+            from leicatca1800 import LeicaTCA1800
+            mu = LeicaTCA1800()
+        elif re.search('550[0-9]$', stationtype):
+            from trimble5500 import Trimble5500
+            mu = Trimble5500()
+
+        iface = SerialIface("rs-232", port)
+        self.ts = TotalStation(stationtype, mu, iface)
+        self.maxtry = maxtry # number of retry if failed
+        self.directions, self.max_faces = self.load(ifname)
+        print self.max_faces
+        if ifname[-4:] == '.geo':
+            self.coordinates, _ = self.load(ifname[:-4] + '.coo')
+        else:
+            self.coordinates, _ = self.load(ifname[:-4] + '.csv')
+        print self.directions
+        print self.coordinates
+        self.station = '???'
+        if 'station' in self.directions[0]:
+            self.station = self.directions[0]['station']
+        self.ih = 0
+        if 'ih' in self.directions[0]:
+            self.ih = self.directions[0]['ih']
+        self.station_east = self.station_north = self.station_elev = 0
+        for coo in self.coordinates:
+            if coo['id'] == self.station:
+                   self.station_east = coo['east']
+                   self.station_north = coo['north']
+                   self.station_elev = coo['elev']
+
+    def load(self, ifn):
+        """ load observation or coordinate data from file
+
+            :param ifn: name of input file
+            :returns: observation list and max faces to measure
+        """
+        # load input data set
+        print ifn
+        if ifn[-4:] in ('.geo', '.coo'):
+            g = GeoReader(fname = ifn)
+        else:
+            g = CsvReader(fname = ifn)
+        directions = []
+        max_faces = 0
+        while 1:
+            w = g.GetNext()
+            if w is None or len(w) == 0:
+                break
+            directions.append(w)
+            if 'faces' in w and max_faces < w['faces']:
+                max_faces = w['faces']
+        return (directions, max_faces)
+
+    def run(self):
+        """ run an observation serie
+        """
+        # wake up instrument
+        self.ts.GetATR()
+        target_msg = "Target on %s point(%s) in face %d and press enter or press 's' to skip the point"
+        n = 0  # number of faces measured fo far
+        while n < self.max_faces:
+            if n % 2 == 0:   # face left
+                i1 = 1
+                i2 = len(self.directions)
+                step = 1
+            else:            # face right
+                i1 = len(self.directions) - 1
+                i2 = 0
+                step = -1
+
+            for i in range(i1, i2, step):
+                if self.directions[i]['faces'] > n:
+                    pn = self.directions[i]['id']
+                    hz = self.directions[i]['hz'].GetAngle()
+                    v = self.directions[i]['v'].GetAngle()
+                    if step < 0:
+                        # change angles to face right
+                        hz = hz - math.pi if hz > math.pi else hz + math.pi 
+                        v = PI2 - v
+                    j = 0   # try count
+                    ans = ''
+                    while j < self.maxtry:
+                        if self.directions[i]['code'][0:3] == 'ATR':
+                            self.ts.SetATR(1)
+                            self.ts.SetEDMMode('STANDARD')
+                            if len(self.directions[i]['code']) > 3:
+                                self.ts.SetPrismType(int(self.directions[i]['code'][3:]))
+                            self.ts.Move(Angle(hz), Angle(v), 1)
+                            self.ts.Measure()
+                        elif self.directions[i]['code'][0:2] == 'PR':
+                            # prism type: 0/1/2/3/4/5/6/7 round/mini/tape/360/user1/user2/user3/360 mini
+                            self.ts.SetATR(0)
+                            self.ts.SetEDMMode('STANDARD')
+                            if len(self.directions[i]['code']) > 2:
+                                self.ts.SetPrismType(int(self.directions[i]['code'][2:]))
+                            self.ts.Move(Angle(hz), Angle(v), 0)
+                            # wait for user to target on point
+                            ans = raw_input(target_msg % (pn, self.directions[i]['code'], n % 2 + 1))
+                            if ans != 's':
+                                self.ts.Measure()
+                        elif self.directions[i]['code'] == 'RL':
+                            self.ts.SetATR(0)
+                            self.ts.SetEDMMode('RLSTANDARD')
+                            self.ts.Move(Angle(hz), Angle(v), 0)
+                            # wait for user to target on point
+                            ans = raw_input(target_msg % (pn, self.directions[i]['code'], n % 2 + 1))
+                            if ans != 's':
+                                self.ts.Measure()
+                        elif self.directions[i]['code'] == 'RLA':
+                            self.ts.SetATR(0)
+                            self.ts.SetEDMMode('RLSTANDARD')
+                            self.ts.Move(Angle(hz), Angle(v), 0)
+                            self.ts.Measure()
+                        elif self.directions[i]['code'] == 'OR':
+                            self.ts.Move(Angle(hz), Angle(v), 0)
+                            # wait for user to target on point
+                            ans = raw_input(target_msg % (pn, self.directions[i]['code'], n % 2 + 1))
+                        else:
+                            j = self.maxtry
+                            break
+                        if ans != 's':
+                            if self.directions[i]['code'] == 'OR':
+                                obs = self.ts.GetAngles()
+                            else:
+                                obs = self.ts.GetMeasure()
+                            if self.ts.measureIface.state != self.ts.measureIface.IF_OK or 'errorCode' in obs:
+                                self.ts.measureIface.state = self.ts.measureIface.IF_OK
+                                j = j + 1
+                                continue
+                            else:
+                                break   # observation OK
+                        else:
+                            j = self.maxtry
+                    if j >= self.maxtry:
+                        print "Cannot measure point %s" % pn
+                        continue
+                    obs['id'] = pn
+                    obs['station'] = self.directions[0]['station']
+                    self.dmp_wrt.WriteData(obs)
+                    coo = {}
+                    if self.directions[i]['code'] != 'OR':
+                        coo['id'] = pn
+                        coo['east'] = self.station_east + obs['distance'] * math.sin(obs['v'].GetAngle()) * math.sin(obs['hz'].GetAngle())
+                        coo['north'] = self.station_north + obs['distance'] * math.sin(obs['v'].GetAngle()) * math.cos(obs['hz'].GetAngle())
+                        coo['elev'] = self.station_elev + self.ih + obs['distance'] * math.cos(obs['v'].GetAngle())
+                        self.coo_wrt.WriteData(coo)
+            n = n + 1
+        # rotate back to first point
+        self.ts.Move(self.directions[1]['hz'], self.directions[1]['v'], 0)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        ifn = sys.argv[1]
     else:
         print ("Usage: robot.py input_file [output_file] [sensor] [serial_port]")
         print ("  or   robot.py config_file.py")
-        exit(-1)
+        ifn = 'test.geo'
+        #exit(-1)
     # output file
     if len(sys.argv) > 2:
-        ofname = sys.argv[2]
-    if ofname[-4:] == '.dmp' or ofname[-4:] == '.csv' or ofname == 'stdout':
-        # filewriter output
-        if ofname[-4:] == '.dmp' or ofname[-4:] == '.csv':
-            ofname1 = ofname[:-4] + '.dmp'
-            ofname2 = ofname[:-4] + '.csv'
-        else:
-            ofname1 = ofname2 = ofname
-        dmp_wrt = CsvWriter(angle = 'DMS', dist = '.4f', \
-            filt = ['station', 'id','hz','v','distance', 'datetime'], \
-            fname = ofname1, mode = 'a', sep = ';')
-        coo_wrt = CsvWriter(dist = '.4f', \
-            filt = ['id', 'east', 'north', 'elev', 'datetime'], \
-            fname = ofname2, mode = 'a', sep = ';')
-    elif ofname[:5] == 'http:' or ofname[:6] == 'https:':
-        # http output
-        ofname1 = ofname2 = ofname
-        dmp_wrt = HttpWriter(angle = 'DMS', dist = '.4f', \
-            filt = ['station', 'id','hz','v','distance', 'datetime'], \
-            url = ofname1, mode = 'POST')
-        coo_wrt = HttpWriter(angle = 'DMS', dist = '.4f', \
-            filt = ['station', 'id','hz','v','distance', 'datetime'], \
-            url = ofname2, mode = 'POST')
+        ofn = sys.argv[2]
     else:
-        print ('unsupported output type\n')
+        #ofn = 'stdout'
+        #ofn = 'http://192.168.1.108/monitoring/get.php'
+        ofn = 'http://192.168.7.145/monitoring/get.php'
+    if ofn[-4:] != '.dmp' and ofn[-4:] != '.csv' and ofn != 'stdout' and ofn[:4] != 'http':
+        print "Unknown output type"
         exit(1)
     if len(sys.argv) > 3:
-        stationtype = sys.argv[3]
-    if re.search('120[0-9]$', stationtype):
-        from leicatps1200 import LeicaTPS1200
-        mu = LeicaTPS1200()
-    elif re.search('110[0-9]$', stationtype):
-        from leicatcra1100 import LeicaTCRA1100
-        mu = LeicaTCRA1100()
-    elif re.search('180[0-9]$', stationtype):
-        from leicatca1800 import LeicaTCA1800
-        mu = LeicaTCA1800()
-    elif re.search('550[0-9]$', stationtype):
-        from trimble5500 import Trimble5500
-        mu = Trimble5500()
+        st = sys.argv[3]
     else:
-        print ("unsupported instrument type")
-        exit(1)
+        st = '1100'
     if len(sys.argv) > 4:
-        port = sys.argv[4]
+        p = sys.argv[4]
+    else:
+        p = '/dev/ttyUSB0'
 
-    iface = SerialIface("rs-232", port)
-    ts = TotalStation(stationtype, mu, iface)
-
-    # load input data set
-    g = GeoReader(fname = ifname)
-    directions = []
-    max_faces = 0
-    # wake up instrument
-    ts.GetATR()
-    while 1:
-        w = g.GetNext()
-        if w is None or len(w) == 0:
-            break
-        directions.append(w)
-        if 'faces' in w and max_faces < w['faces']:
-            max_faces = w['faces']
-
-    maxtry = 3 # number of retry if failed
-    n = 0  # number of faces measured fo far
-    while n < max_faces:
-        if n % 2 == 0:   # face left
-            i1 = 1
-            i2 = len(directions)
-            step = 1
-        else:            # face right
-            i1 = len(directions) - 1
-            i2 = 0
-            step = -1
-
-        for i in range(i1, i2, step):
-            if directions[i]['faces'] > n:
-                pn = directions[i]['pid']
-                hz = directions[i]['hz'].GetAngle()
-                v = directions[i]['v'].GetAngle()
-                if step < 0:
-                    # change angles to face right
-                    hz = hz - math.pi if hz > math.pi else hz + math.pi 
-                    v = PI2 - v
-                j = 0   # try count
-                ans = ''
-                while j < maxtry:
-                    if directions[i]['code'][0:3] == 'ATR':
-                        ts.SetATR(1)
-                        ts.SetEDMMode('STANDARD')
-                        if len(directions[i]['code']) > 3:
-                            ts.SetPrismType(int(directions[i]['code'][3:]))
-                        ts.Move(Angle(hz), Angle(v), 1)
-                        ts.Measure()
-                    elif directions[i]['code'][0:2] == 'PR':
-                        # prism type: 0/1/2/3/4/5/6/7 round/mini/tape/360/user1/user2/user3/360 mini
-                        ts.SetATR(0)
-                        ts.SetEDMMode('STANDARD')
-                        if len(directions[i]['code']) > 2:
-                            ts.SetPrismType(int(directions[i]['code'][2:]))
-                        ts.Move(Angle(hz), Angle(v), 0)
-                        # wait for user to target on point
-                        ans = raw_input("Target on %s point(%s) in face %d and press enter or press 's' to skip the point" % (pn, directions[i]['code'], n % 2 + 1))
-                        if ans != 's':
-                            ts.Measure()
-                    elif directions[i]['code'] == 'RL':
-                        ts.SetATR(0)
-                        ts.SetEDMMode('RLSTANDARD')
-                        ts.Move(Angle(hz), Angle(v), 0)
-                        # wait for user to target on point
-                        ans = raw_input("Target on %s point(%s) in face %d and press enter" % (pn, directions[i]['code'], n % 2 + 1))
-                        if ans != 's':
-                            ts.Measure()
-                    elif directions[i]['code'] == 'OR':
-                        ts.Move(Angle(hz), Angle(v), 0)
-                        # wait for user to target on point
-                        ans = raw_input("Target on %s point(%s) in face %d and press enter or press 's' to skip the point" % (pn, directions[i]['code'], n % 2 + 1))
-                    else:
-                        print ("Unknow code")
-                        j = maxtry
-                        break
-                    if ans != 's':
-                        if directions[i]['code'] == 'OR':
-                            obs = ts.GetAngles()
-                        else:
-                            obs = ts.GetMeasure()
-                        if ts.measureIface.state != ts.measureIface.IF_OK or \
-                            'errorCode' in obs:
-                            ts.measureIface.state = ts.measureIface.IF_OK
-                            j = j + 1
-                            continue
-                        else:
-                            break   # observation OK
-                    else:
-                        j = maxtry
-                if j >= maxtry:
-                    print "Cannot measure point %s" % pn
-                    continue
-                obs['id'] = pn
-                obs['station'] = directions[0]['station']
-                dmp_wrt.WriteData(obs)
-                if directions[i]['code'] != 'OR':
-                    coo = {}
-                    coo['id'] = pn
-                    coo['east'] = obs['distance'] * math.sin(obs['v'].GetAngle()) * \
-                        math.sin(obs['hz'].GetAngle())
-                    coo['north'] = obs['distance'] * math.sin(obs['v'].GetAngle()) * \
-                        math.cos(obs['hz'].GetAngle())
-                    coo['elev'] = obs['distance'] * math.cos(obs['v'].GetAngle())
-                    coo_wrt.WriteData(coo)
-        n = n + 1
-    # rotate back to first point
-    ts.Move(directions[1]['hz'], directions[1]['v'], 0)
+    r = Robot(ifn, ofn, st, p)
+    r.run()
