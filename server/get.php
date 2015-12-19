2d8d9cb8356b2383e1b567076bbf7da83e5fa102
zsiki <siki@agt.bme.hu>
2015-12-19 17:05:39 +0100

    replace symbolic link with file

--- server/get.php
+++ server/get.php
@@ -1,1 +1,57 @@
-/home/html/monitoring/get.php
+<?php
+/*
+ * Sample script to collect data from ulyxes monitoring (httpwriter)
+ *
+ */
+
+	//error_log(http_build_query($_REQUEST));
+
+	$STR_TYPE = array('varchar', 'char', 'timestamp');
+	include_once("config.php");
+
+	if (isset($_REQUEST['east']) && isset($_REQUEST['north']) &&
+		isset($_REQUEST['elev']) && isset($_REQUEST['datetime'])) {
+		// coordinate record sent
+		$table =$coo_table;
+	} elseif (isset($_REQUEST['hz']) && isset($_REQUEST['v']) &&
+		isset($_REQUEST['distance']) && isset($_REQUEST['datetime'])) {
+		// totalstation observation record sent
+		$table = $obs_table;
+	} else {
+		echo -1;
+		error_log("Parameter error:" . http_build_query($_REQUEST));
+		exit();
+	}
+	// $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=ulyxes;user=postgres;password=qwerty');
+	$dbh = new PDO($conn_str);
+	if (! $dbh){
+		echo -2;	// connection error
+		error_log("Connection error: check connection string");
+		exit();
+	}
+	// collect column names from table
+	$rs = $dbh->query("SELECT * FROM $table LIMIT 0");
+	for ($i = 0; $i < $rs->columnCount(); $i++) {
+		$col = $rs->getColumnMeta($i);
+	    $columns[] = $col['name'];
+		$types[$col['name']] = $col['native_type'];
+	}
+	// collect column name and value pairs for insert
+	$cols = "";		// columns in insert
+	$vals = "";		// values in insert
+	foreach ($_REQUEST as $key => $value) {
+		if (in_array($key, $columns)) {
+			$cols .= $key . ',';
+			if (in_array($types[$key], $STR_TYPE)) {
+				$vals .= "'" . $value . "',";
+			} else {
+				$vals .= $value . ',';
+			}
+		}
+	}
+	$cols = trim($cols, ',');
+	$vals = trim($vals, ',');
+	$i = $dbh->exec("insert into $table ($cols) values ($vals)");
+	//error_log("insert into $table ($cols) values ($vals)");
+	echo $i;	// number of lines affected (1)
+?>
--- server/monitoring.sql
+++ server/monitoring.sql
@@ -1,1 +1,33 @@
-/home/html/monitoring/monitoring.sql
+-- tables for Ulyxes monitoring
+
+-- drop previous version of tables
+DROP TABLE IF EXISTS monitoring_coo;
+DROP TABLE IF EXISTS monitoring_obs;
+SELECT DropGeometryTable('monitoring_poi');
+DROP TABLE IF EXISTS monitoring_poi;
+
+-- table for points with web mercator geometry
+CREATE TABLE monitoring_poi (
+	id varchar(50) PRIMARY KEY
+);
+SELECT AddGeometryColumn('monitoring_poi', 'geom', 3857, 'POINT', 3);
+
+-- table for point coordinates in local reference system
+CREATE TABLE monitoring_coo (
+	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
+	east double precision NOT NULL,
+	north double precision NOT NULL,
+	elev double precision NOT NULL,
+	datetime timestamp NOT NULL,
+	CONSTRAINT pkey_coo PRIMARY KEY (id, datetime)
+);
+
+-- table for observations
+CREATE TABLE monitoring_obs (
+	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
+	hz double precision NOT NULL,
+	v double precision NOT NULL,
+	distance double precision NOT NULL,
+    datetime timestamp NOT NULL,
+	CONSTRAINT pkey_obs PRIMARY KEY (id, datetime)
+);
