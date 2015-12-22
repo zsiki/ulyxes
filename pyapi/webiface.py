dbdf3a3336e2a8991a9b33fdff206c1c20cf3329
zsiki <siki@agt.bme.hu>
2015-12-22 16:26:07 +0100

    initial commit

---
+++ pyapi/webiface.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. module:: webiface.py
+   :platform: Unix, Windows
+   :synopsis: Ulyxes - an open source project to drive total stations and
+       publish observation results.  GPL v2.0 license Copyright (C)
+       2010-2015 Zoltan Siki <siki@agt.bme.hu>.
+
+.. moduleauthor:: Zoltan Siki <siki@agt.bme.hu>
+
+"""
+
+import urllib2
+import json
+from iface import Iface
+
+class WebIface(Iface):
+    """Class for communicating web page using HTTP GET and json/txt output.
+
+        :param name: name of interface (str)
+        :param url: address to read from (str)
+    """
+
+    def __init__(self, name, url, fmt):
+        """Create an instance of the web interface
+        """
+        super(WebIface, self).__init__(name)
+        self.url = url
+        self.fmt = fmt
+
+    def Send(self, msg):
+        """ send message to web
+
+            :param msg: parameters to url
+            :returns: downloaded data
+        """
+        response = urllib2.urlopen(self.url + '?' + msg)
+        if self.fmt == 'json':
+            data = json.load(response)
+        else:
+            data = response.read()
+        return data
+
+if __name__ == "__main__":
+    w = WebIface("demo", "http://api.openweathermap.org/data/2.5/weather", "json")
+    print w.Send("q=budapest&appid=13152b0308b85a39cc9a161e241ec2cf")
+    w = WebIface("demo", "http://www.geod.bme.hu/on_line/etrs2eov/etrs2eov.php", "txt")
+    print w.Send("e=650000&n=240000&sfradio=single&format=TXT")
