8476e360e77087e65345d3349f029e9b03b3101a
zsiki <siki@agt.bme.hu>
2016-01-08 11:27:40 +0100

    typo

--- pyapi/leicatps1200.py
+++ pyapi/leicatps1200.py
@@ -63,7 +63,7 @@ class LeicaTPS1200(LeicaMeasureUnit):
         'SEARCHNEXT': 9051,
         'SETREDLASER': 1004,
         'GETPT': 17009,
-        'SETPT': 17008
+        'SETPT': 17008,
         'GETSPIRAL': 9040,
         'SETSPIRAL': 9041,
         'SEARCHTARGET': 17020
