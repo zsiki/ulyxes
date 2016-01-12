a11fd8b40b99fb07003fad470f7e4e76587ff7d2
zsiki <siki@agt.bme.hu>
2016-01-12 11:25:09 +0100

    ptyp added to monitoring_poi table

--- server/monitoring.sql
+++ server/monitoring.sql
@@ -7,12 +7,17 @@ SELECT DropGeometryTable('monitoring_poi');
 DROP TABLE IF EXISTS monitoring_poi;
 
 -- table for points with web mercator geometry
+-- each point must be inserted into this table
+-- it is used to display points on web map (EPSG:3857)
+-- ptype can be FIX/STA/MON for fix points, stations, monitoring points
 CREATE TABLE monitoring_poi (
 	id varchar(50) PRIMARY KEY
+	ptype char(3) NOT NULL DEFAULT 'MON' CHECK (ptype in ('FIX','STA','MON')),
 );
 SELECT AddGeometryColumn('monitoring_poi', 'geom', 3857, 'POINT', 3);
 
 -- table for point coordinates in local reference system
+-- coordinates calculated by Ulyxes
 CREATE TABLE monitoring_coo (
 	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
 	east double precision NOT NULL,
@@ -23,6 +28,7 @@ CREATE TABLE monitoring_coo (
 );
 
 -- table for observations
+-- polar observations made by Ulyxes
 CREATE TABLE monitoring_obs (
 	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
 	hz double precision NOT NULL,
