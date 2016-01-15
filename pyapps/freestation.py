287e77ce41fb8e98eb8ebfd4a59fc72cca62dc5f
zsiki <siki@agt.bme.hu>
2016-01-15 02:27:23 +0100

    redesing class

--- pyapps/freestation.py
+++ pyapps/freestation.py
@@ -27,74 +27,38 @@ from gamaiface import GamaIface
 #logging.getLogger().setLevel(logging.WARNING)
 logging.getLogger().setLevel(logging.INFO)
 
-#fname = "/home/siki/GeoEasy/data/freestation.geo"
-fname = "test.geo"
-gama_path = '/home/siki/GeoEasy/gama-local'
-
 class Freestation(object):
     """ Calculate freestation and remove blunders
 
-        :param ifname: input geo/coo or dmp/csv file name
-        :param station_id: station id, default first station in observation file
-        :param istrument_height: instrument height, default 0
+        :param obs: list of observations
+        :param coords: coordinates of points
+        :param gama_path: path to gama-local
     """
 
-    def __init__(self, fname, gama_path):
+    def __init__(self, obs, coords, gama_path):
         """ initialize
         """
         # create gama interface
         self.g = GamaIface(gama_path, 3, 0.95, 1, 1, 1.5)
-        # load observations and coordinates
-        fn = fname[:-4] # remove extension
-        ext = fname[-4:]
-        if ext in ['.geo', '.coo']:
-            obs = GeoReader(fname = fn + '.geo')
-        else:
-            obs = CsvReader(fname = fn + '.dmp')
-        # load observations
-        no = 0  # number of observations
-        ns = 0  # number of stations
-        while True:
-            w = obs.GetNext()
-            if w is None or len(w) == 0:
-                break
+        ns = 0    # number of stations
+        no = 0    # number of observations
+        self.station = None
+        for w in obs:
             if 'station' in w:
                 ns += 1
                 if ns > 1:
-                    break   # only first station is used
-                station = w['station']
+                    break    # stop after first station
+                self.station = w['station']
                 self.g.add_observation(w)
             elif 'id' in w and 'hz' in w and 'v' in w and 'distance' in w:
                 no += 1 # number of observations
                 self.g.add_observation(w)
-        if no < 3:
-            logging.error("few observations")
-        # load coordinates and add to adjustment
-        if ext in ['.geo', '.coo']:
-            coo = GeoReader(fname = fn + '.coo')
-        else:
-            coo = CsvReader(fname = fn + '.csv')
-        n = 0   # number of points
-        st = False  # station found
-        while True:
-            w = coo.GetNext()
-            if w is None or len(w) == 0:
-                break
-            n += 1
-            if 'id' in w:
-                if w['id'] == station:
-                    self.g.add_point(w, 'ADJ')
-                    st = True
-                elif 'east' in w and 'north' in w and 'elev' in w:
-                    self.g.add_point(w, 'FIX')
-                else:
-                    logging.warning("point skiped (no 3D coords):" + w['id'])
-            else:
-                logging.warning("line skiped (no id):" + str(n))
-        if n < 3:
-            logging.error("not enough points found in coordinate list")
-        if not st:
-            logging.error("not station found in coordinate list:" + station)
+        for w in coords:
+            if w['id'] == self.station:
+                self.g.add_point(w, 'ADJ')
+            elif 'east' in w and 'north' in w and 'elev' in w:
+                self.g.add_point(w, 'FIX')
+        # TODO if no < 3
 
     def Adjustment(self):
         """ adjustment & and blunder removing
@@ -118,19 +82,41 @@ class Freestation(object):
         return res
 
 if __name__ == "__main__":
+    #fname = "/home/siki/GeoEasy/data/freestation.geo"
+    #fname = "test.geo"
+    #gama_path = '/home/siki/GeoEasy/gama-local'
+
     if len(sys.argv) > 1:
         fname = sys.argv[1]
         if not os.path.isfile(fname):
             print "File not found: " + fname
-            exit(-1)
+            sys.exit(-1)
     else:
         print "Usage: freestation.py input_file gama_path station_id station_height"
-        exit(-1)
+        sys.exit(-1)
     if not fname[-4:] in ['.geo', '.coo', '.dmp', '.csv']:
         fname += '.geo'
     if len(sys.argv) > 2:
         gama_path = sys.argv[2]
     else:
         gama_path = '/home/siki/GeoEasy/gama-local'
-    f = Freestation(fname, gama_path)
+
+    # load observations and coordinates
+    fn = fname[:-4] # remove extension
+    ext = fname[-4:]
+    if ext in ['.geo', '.coo']:
+        obs = GeoReader(fname = fn + '.geo')
+    else:
+        obs = CsvReader(fname = fn + '.dmp')
+    # load observations
+    observations = obs.Load()
+    # load coordinates and add to adjustment
+    if ext in ['.geo', '.coo']:
+        coo = GeoReader(fname = fn + '.coo')
+    else:
+        coo = CsvReader(fname = fn + '.csv')
+    n = 0   # number of points
+    st = False  # station found
+    coords = coo.Load()
+    f = Freestation(observations, coords, gama_path)
     print f.Adjustment()
