79865e50a14dd4924f72bb0ebf28f16855eb3a39
zsiki <siki@agt.bme.hu>
2017-09-03 15:50:00 +0200

    SqLiteWriter initial commit

---
+++ pyapi/sqlitewriter.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+"""
+.. module:: sqlitewriter.py
+   :platform: Unix, Windows
+   :synopsis: Ulyxes - an open source project to drive total stations and
+       publish observation results.
+       GPL v2.0 license
+       Copyright (C) 2010-2013 Zoltan Siki <siki@agt.bme.hu>
+
+.. moduleauthor:: Zoltan Siki <siki@agt.bme.hu>
+"""
+
+import os.path
+import logging
+import datetime
+import sqlite3
+from angle import Angle
+from writer import Writer
+
+class SqLiteWriter(Writer):
+    """ Class to write observations to a local sqlite database
+
+            :param name: name of writer (str)
+            :param angle: angle unit to use (str), DMS not supported
+            :param dist: distance and coordinate format (str)
+            :param dt: date/time format (str), default ansi
+            :param filt: list of allowed keys (list)
+            :param db: name of database file (str)
+    """
+
+    tables = {'coo': 'monitoring_coo', 'obs': 'monitoring_obs', 'met': 'monitoring_met'}
+
+    def __init__(self, db, name = None, angle = 'GON', dist = '.3f', 
+                dt = '%Y-%m-%d %H:%M:%S', filt = None):
+        """ Constructor
+        """
+        if angle == 'DMS':
+            angel = 'GON'
+            logging.warning('Angle type changed from DMS to GON')
+        super(SqLiteWriter, self).__init__(name, angle, dist, dt, filt)
+        if os.path.isfile(db):
+            self.db = db
+            # connect to local db
+            self.conn = sqlite3.connect(db)
+        else:
+            logging.error('SqLite database does not exists: ' + db)
+
+    def __del__(self):
+        try:
+            self.conn.close()
+        except:
+            pass
+
+    def WriteData(self, data):
+        """ Write observation data to db
+
+            :param data: dictionary with observation data
+        """
+        res = 0
+        par = {}
+        if data is None or self.DropData(data):
+            logging.warning(" empty or inappropiate data not written")
+            return
+        # add datetime and/or id
+        data = self.ExtendData(data)
+        # select table
+        if 'hz' in data:
+            table = self.tables['obs']
+        elif 'temp'in data:
+            table = self.tables['met']
+        elif 'east' in data or 'elev' in data:
+            table = self.tables['coo']
+        # build sql statement
+        fields = ""
+        values = ""
+        for key, val in data.items():
+            if self.filt is None or key in self.filt:
+                fields += key + ','
+                if type(val) is str or type(val) is type(datetime.datetime.now()):
+                    values += "'" + self.StrVal(val) + "'"
+                else:
+                    values += self.StrVal(val)
+                values += ','
+        sqlstr = 'INSERT INTO ' + table + '(' + fields[:-1] + ')' + \
+            ' VALUES (' + values[:-1] + ');'
+        c = self.conn.cursor()
+        c.execute(sqlstr)
+        # TODO error handling-> res
+        self.conn.commit()
+        return res
+
+if __name__ == "__main__":
+    myfile = SqLiteWriter(db = "test.sqlite")
+    data = {'id': '1', 'hz': Angle(0.12345), 'v': Angle(100.2365, 'GON'), 'distance': 123.6581}
+    print (myfile.WriteData(data))
+    data = {'id': '1', 'east': 0.12345, 'north': 100.2365, 'elev': 123.6581}
+    print (myfile.WriteData(data))
+    data = {'id': '1', 'temp': 12.45, 'pressure': 1017}
+    print (myfile.WriteData(data))
---
+++ server/monitoring.sqlite.sql
@@ -0,0 +1,76 @@
+-- tables for Ulyxes monitoring
+-- spatialite version
+-- create a new empty spatialite database first
+
+-- table for points with web mercator geometry
+-- each point must be inserted into this table
+-- it is used to display points on web map (EPSG:3857)
+-- ptype can be FIX/STA/MON for fix points, stations, monitoring points
+-- code can be ATR/RLA/PR/RL/ORI
+-- pc additive constant for distance measurement
+CREATE TABLE monitoring_poi (
+	id varchar(50) PRIMARY KEY,
+	ptype char(3) NOT NULL DEFAULT 'MON' CHECK (ptype in ('FIX','STA','MON')),
+	code char(4) NOT NULL DEFAULT 'ATR' CHECK (code in ('ATR', 'PR', 'ORI', 'RL', 'RLA')),
+	pc float NOT NULL DEFAULT 0
+);
+SELECT AddGeometryColumn('monitoring_poi', 'geom', 3857, 'POINT', 3);
+
+-- table for point coordinates in local reference system
+-- coordinates calculated by Ulyxes
+CREATE TABLE monitoring_coo (
+	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
+	east double precision NOT NULL,
+	north double precision NOT NULL,
+	elev double precision NOT NULL,
+	datetime timestamp NOT NULL,
+	CONSTRAINT pkey_coo PRIMARY KEY (id, datetime)
+);
+
+-- table for observations
+-- polar observations made by Ulyxes
+CREATE TABLE monitoring_obs (
+	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
+	hz double precision NOT NULL,
+	v double precision NOT NULL,
+	distance double precision,
+	crossincline double precision,
+	lengthincline double precision,
+    datetime timestamp NOT NULL,
+	CONSTRAINT pkey_obs PRIMARY KEY (id, datetime)
+);
+
+-- table for meteorological observations at stations
+-- id point id for observation
+-- temp temperature celsius
+-- pressure air pressure hpa
+-- huminidity
+-- wettemp wet temperature
+-- datetime of observation
+CREATE TABLE monitoring_met (
+	id varchar(50) NOT NULL REFERENCES monitoring_poi(id),
+	temp double precision NOT NULL,
+	pressure double precision NOT NULL,
+	huminidity double precision,
+	wettemp double precision,
+    datetime timestamp NOT NULL,
+	CONSTRAINT pkey_m PRIMARY KEY (id, datetime)
+);
+
+-- table for point groups
+-- id name of group
+-- remark optional description of group
+CREATE TABLE monitoring_grp (
+	id varchar(50) PRIMARY KEY,
+	remark varchar(100)
+);
+
+-- table for point group connections
+-- gid group id
+-- pid point id
+CREATE TABLE monitoring_pgr (
+	gid varchar(50) NOT NULL REFERENCES monitoring_grp(id),
+	pid varchar(50) NOT NULL REFERENCES monitoring_poi(id),
+	CONSTRAINT pkey_pgr PRIMARY KEY (gid, pid)
+);
+
