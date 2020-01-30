4b5eeac776d1f3a70fe8f52f641be662a690faf6
zsiki <siki1958@gmail.com>
2020-01-30 17:11:52 +0100

    new classes added

--- pyapi/__init__.py
+++ pyapi/__init__.py
@@ -2,14 +2,21 @@
 """
 __all__ = [
 # general
-"angle", "reader",
+"angle",
+# readers
+"reader", "confreader", "csvreader", "filereader", "georeader", 
+"httpreader", "jsonreader", "sqlitereader", 
 # interfaces
-"iface", "localiface", "serialiface", "videoiface",
+"iface", "localiface", "serialiface", "videoiface", "bluetoothiface",
+"gamaiface", "i2ciface", "picamiface", "webiface", 
 # measure units
 "measureunit", "leicameasureunit", "leicatca1800", "leicatps1200",
 "trimble5500", "leicadnaunit", "nmeagnssunit", "videomeasureunit",
+"bmp180measureunit", "leicatcra1100", "lsm9ds0unit", "webmetmeasureunit",
+"wifiunit",
 # writers
 "writer", "echowriter", "filewriter", "csvwriter", "imagewriter", 
-"videowriter", "httpwriter"
+"videowriter", "httpwriter", "geowriter", "sqlitewriter", 
 # instruments/sensors
-"instrument", "totalstation", "digitallevel", "webcam", "gnss"]
+"instrument", "totalstation", "digitallevel", "webcam", "gnss", "bmp180",
+"lsm9ds0", "webmet", "wificollector" ]
