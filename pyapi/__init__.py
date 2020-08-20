c9419b52d42b36145887b6eb95a7b215992c84e3
zsiki <siki1958@gmail.com>
2020-08-20 16:17:21 +0200

    preparing PyPi package

--- pyapi/__init__.py
+++ pyapi/__init__.py
@@ -5,18 +5,18 @@ __all__ = [
 "angle",
 # readers
 "reader", "confreader", "csvreader", "filereader", "georeader", 
-"httpreader", "jsonreader", "sqlitereader", 
+"httpreader", "jsonreader", "sqlitereader", "queuereader",
 # interfaces
 "iface", "localiface", "serialiface", "videoiface", "bluetoothiface",
-"gamaiface", "i2ciface", "picamiface", "webiface", 
+"gamaiface", "i2ciface", "picamiface", "webiface", "tcpiface",
 # measure units
 "measureunit", "leicameasureunit", "leicatca1800", "leicatps1200",
 "trimble5500", "leicadnaunit", "nmeagnssunit", "videomeasureunit",
 "bmp180measureunit", "leicatcra1100", "lsm9ds0unit", "webmetmeasureunit",
-"wifiunit",
+"wifiunit", "picameraunit", "remotemeasureunit.py",
 # writers
 "writer", "echowriter", "filewriter", "csvwriter", "imagewriter", 
-"videowriter", "httpwriter", "geowriter", "sqlitewriter", 
+"videowriter", "httpwriter", "geowriter", "sqlitewriter", "queuewriter",
 # instruments/sensors
 "instrument", "totalstation", "digitallevel", "webcam", "gnss", "bmp180",
-"lsm9ds0", "webmet", "wificollector" ]
+"lsm9ds0", "webmet", "wificollector", "camera", "camerastation", "sensehat" ]
