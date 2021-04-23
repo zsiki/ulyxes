1d06e28b636423d8330aed0f48fa2f0f7356ffbe
zsiki <siki1958@gmail.com>
2021-04-23 10:16:03 +0200

    headerless CSV handling with fields parameter

--- pyapi/csvreader.py
+++ pyapi/csvreader.py
@@ -20,19 +20,23 @@ class CsvReader(FileReader):
             :param fname: name of input file
             :param separator: field separator, default ;
             :param filt: list of fields to use, default None (use all)
+            :param fields: filed names for columns in CSV if not in the first line
     """
 
-    def __init__(self, name=None, fname=None, separator=';', filt=None):
+    def __init__(self, name=None, fname=None, separator=';', filt=None, fields=None):
         """ Constructor
         """
         super(CsvReader, self).__init__(name, fname, filt)
         self.separator = separator
         self.filt = filt
-        # get field names from header line
-        self.fields = []
-        if self.state == self.RD_OK:
-            self.fields = [
-                x.strip() for x in self.GetLine().split(self.separator)]
+        if fields is None:
+            # get field names from header line
+            self.fields = []
+            if self.state == self.RD_OK:
+                self.fields = [
+                    x.strip() for x in self.GetLine().split(self.separator)]
+        else:
+            self.fields = fields
 
     def __del__(self):
         """ Destructor
@@ -58,8 +62,9 @@ class CsvReader(FileReader):
         return res
 
 if __name__ == '__main__':
-    cr = CsvReader('test', 'test.csv')
+    cr = CsvReader('test', '../data/elev_1056.csv', separator=',',
+                   fields=['psz', 'dt', 'east', 'north', 'elev', 'code'])
     if cr.state == cr.RD_OK:
-        print (cr.Load())
+        print(cr.Load())
     else:
         print("File not found")
