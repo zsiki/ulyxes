c98ac0c8da19493db5f3b7471347f8d69beeca58
zsiki <siki1958@gmail.com>
2021-09-20 15:53:47 +0200

    find multiple markers

--- camera/camera_aruco.py
+++ camera/camera_aruco.py
@@ -3,8 +3,29 @@
 """
     go through video frames to find an aruco code
     video file name can contain date and time like: pi1_YYYYmmdd_HHMMSS.h264
-    output sent to standard output in the form:
-    line,date/time,column,row,statistic
+    parameters are given in a json file
+
+    JSON parameters:
+        log_file: path to log file (file must exist)
+        log_level: 10/20/30/40/50 DEBUG/INFO/WARNING/ERROR/FATAL
+        log_format: format for log entries
+        station_id: station point ID, optional (str)
+        camera_id: camera ID, oprtional (str)
+        fps: frame/sec in video file or sampling rate from camera
+        dict: ArUco dictionary, default 1 (4x4_100)
+        code: ArUco code to search
+        size: size of ArUco marked in mm, external size of black border
+        calibration: calibration file in yaml format
+        hist: histogram enhancement of image, optional (int 0/1), default 0
+        lchanel: use l chanel of image, optional, (int 0/1), default 0
+        clip: for adaptive threshold, optional, (float), default 3.0
+        tile: for adaptive threshold, optional, (int), default 8
+        coo_wr: coordinate output (str)
+            csv: path to putput file
+            sqlite database: path to sqlite database file
+            postgres database: connection string to pg
+            http: url to remote application over http
+        debug: visual feedback, optional (int 0/1), default 0
 
     usage: camera_arucoco.py config.json
 """
@@ -14,6 +35,7 @@ import sys
 import os.path
 import logging
 import time
+import cv2
 import matplotlib.pyplot as plt
 from aruco_base import ArucoBase
 
@@ -66,34 +88,33 @@ class CameraAruco(ArucoBase):
                 delay_time = t_gap - (t_end - t_start)
                 if delay_time > 0.1:
                     print("Sleeping ", delay_time)
-                    time.sleep(delay_time)
+                    #time.sleep(delay_time)
+                    key = cv2.waitKey(int(delay_time * 1000)) #& 0xFF
+                    print('key: ', key)
+                    if key == ord('q'):
+                        break
             t_start = time.perf_counter()
             frame, frame_time = self.rdr.GetNext() # get next frame
             print("New frame at ", str(frame_time))
             if frame is not None:
-                res = self.ProcessImg(frame, self.rdr.ind)
-                if res:
+                results = self.ProcessImg(frame, self.rdr.ind)
+                for res in results:
                     if self.size is not None:
                         scale = self.size / max(res['width'], res['height'])
                     if self.calibration:    # output pose, too
-                        data = {'id': self.code, 'datetime': frame_time,
+                        data = {'id': res['code'], 'datetime': frame_time,
                                 'east': res["east"] * scale,
                                 'elev': res["north"] * scale,
                                 'width': res['width'], 'height': res['height'],
-                                'code': self.code,
                                 'roll': res["euler_angles"][0],
                                 'pitch': res["euler_angles"][1],
                                 'yaw': res["euler_angles"][2]}
                     else:
-                        data = {'id': self.code, 'datetime': frame_time,
+                        data = {'id': res['code'], 'datetime': frame_time,
                                 'east': res["east"] * scale,
                                 'elev': res["north"] * scale,
-                                'width': res['width'], 'height': res['height'],
-                                'code': self.code}
+                                'width': res['width'], 'height': res['height']}
                     self.wrt.WriteData(data)
-                else:   # no marker found search whole image next
-                    self.last_x = self.last_y = None
-                    self.off_y = self.off_x = 0
             else:
                 break
             t_end = time.perf_counter()
