d25d99599928e1b98eef8e7318c3e19455466a77
zsiki <siki1958@gmail.com>
2023-11-30 22:22:07 +0100

    dictionary id added as comment

--- camera/aruco_dict.py
+++ camera/aruco_dict.py
@@ -1,25 +1,25 @@
 from cv2 import aruco
 
 ARUCO_DICT = {
-    "DICT_4X4_50": aruco.DICT_4X4_50,
-    "DICT_4X4_100": aruco.DICT_4X4_100,
-    "DICT_4X4_250": aruco.DICT_4X4_250,
-    "DICT_4X4_1000": aruco.DICT_4X4_1000,
-    "DICT_5X5_50": aruco.DICT_5X5_50,
-    "DICT_5X5_100": aruco.DICT_5X5_100,
-    "DICT_5X5_250": aruco.DICT_5X5_250,
-    "DICT_5X5_1000": aruco.DICT_5X5_1000,
-    "DICT_6X6_50": aruco.DICT_6X6_50,
-    "DICT_6X6_100": aruco.DICT_6X6_100,
-    "DICT_6X6_250": aruco.DICT_6X6_250,
-    "DICT_6X6_1000": aruco.DICT_6X6_1000,
-    "DICT_7X7_50": aruco.DICT_7X7_50,
-    "DICT_7X7_100": aruco.DICT_7X7_100,
-    "DICT_7X7_250": aruco.DICT_7X7_250,
-    "DICT_7X7_1000": aruco.DICT_7X7_1000,
-    "DICT_ARUCO_ORIGINAL": aruco.DICT_ARUCO_ORIGINAL,
-    "DICT_APRILTAG_16h5": aruco.DICT_APRILTAG_16h5,
-    "DICT_APRILTAG_25h9": aruco.DICT_APRILTAG_25h9,
-    "DICT_APRILTAG_36h10": aruco.DICT_APRILTAG_36h10,
-    "DICT_APRILTAG_36h11": aruco.DICT_APRILTAG_36h11
+    "DICT_4X4_50": aruco.DICT_4X4_50,                       # 0
+    "DICT_4X4_100": aruco.DICT_4X4_100,                     # 1
+    "DICT_4X4_250": aruco.DICT_4X4_250,                     # 2
+    "DICT_4X4_1000": aruco.DICT_4X4_1000,                   # 3
+    "DICT_5X5_50": aruco.DICT_5X5_50,                       # 4
+    "DICT_5X5_100": aruco.DICT_5X5_100,                     # 5
+    "DICT_5X5_250": aruco.DICT_5X5_250,                     # 6
+    "DICT_5X5_1000": aruco.DICT_5X5_1000,                   # 7
+    "DICT_6X6_50": aruco.DICT_6X6_50,                       # 8
+    "DICT_6X6_100": aruco.DICT_6X6_100,                     # 9
+    "DICT_6X6_250": aruco.DICT_6X6_250,                     #10
+    "DICT_6X6_1000": aruco.DICT_6X6_1000,                   #11
+    "DICT_7X7_50": aruco.DICT_7X7_50,                       #12
+    "DICT_7X7_100": aruco.DICT_7X7_100,                     #13
+    "DICT_7X7_250": aruco.DICT_7X7_250,                     #14
+    "DICT_7X7_1000": aruco.DICT_7X7_1000,                   #15
+    "DICT_ARUCO_ORIGINAL": aruco.DICT_ARUCO_ORIGINAL,       #16
+    "DICT_APRILTAG_16h5": aruco.DICT_APRILTAG_16h5,         #17
+    "DICT_APRILTAG_25h9": aruco.DICT_APRILTAG_25h9,         #18
+    "DICT_APRILTAG_36h10": aruco.DICT_APRILTAG_36h10,       #19
+    "DICT_APRILTAG_36h11": aruco.DICT_APRILTAG_36h11        #20
 }
