14f46c56a357d4f64b9b3df9036d985a49457274
zsiki <siki1958@gmail.com>
2023-11-30 22:29:41 +0100

    pose cli parameter added

--- camera/aruco_base.py
+++ camera/aruco_base.py
@@ -9,25 +9,24 @@ from math import (sqrt, atan2, hypot)
 from os import path
 import sys
 import logging
+import json
 import matplotlib.pyplot as plt
 import numpy as np
-import json
 import yaml
 import cv2
+from confreader import ConfReader
 from aruco_dict import ARUCO_DICT
 
 # check PYTHONPATH
 if len([p for p in sys.path if 'pyapi' in p]) == 0:
-    if os.path.isdir('../pyapi/'):
+    if path.isdir('../pyapi/'):
         sys.path.append('../pyapi/')
     else:
         print("pyapi not found")
         print("Add pyapi directory to the Python path or start your application from ulyxes/pyapps folder")
         sys.exit(1)
 
-from confreader import ConfReader
-
-# handle incompatibility introduced in openCV 4.8 
+# handle incompatibility introduced in openCV 4.8
 if cv2.__version__ < '4.8':
     cv2.aruco.extendDictionary = cv2.aruco.Dictionary_create
     cv2.aruco.getPredefinedDictionary = cv2.aruco.Dictionary_get
@@ -67,7 +66,7 @@ class ArucoBase():
         else:
             # read params from json
             try:
-                with open(args.aruco_params) as f:
+                with open(args.aruco_params, encoding='ascii') as f:
                     data = f.read()
                 js = json.loads(data)
             except:
@@ -89,10 +88,11 @@ class ArucoBase():
             'station_id': {'required' : False, 'type': 'str'},
             'camera_id': {'required' : False, 'type': 'str', 'default': '1'},
             'fps': {'required': False, 'type': 'float', 'default': 1},
-            'dict': {'required': False, 'type': 'int', 'default': 1},
+            'dict': {'required': False, 'type': 'str', 'default': 1},
             'code': {'required': False, 'type': 'int', 'default': None},
             'size': {'required': False, 'type': 'float', 'default': 100.0},
             'calibration': {'required': False, 'type': 'file', 'default': None},
+            'pose': {'required': False, 'type': 'bool', 'default': False},
             'hist': {'required': False, 'type': 'int', 'default': 0},
             'refine': {'required': False, 'type': 'int', 'default': 0},
             'lchanel': {'required': False, 'type': 'int', 'default': 0},
@@ -112,24 +112,30 @@ class ArucoBase():
         if not cr.Check():
             print("Config check failed")
             sys.exit(-1)
-        if cr.json['dict'] == 99:     # use special 3x3 dictionary
-            try:    # for 4.8 compatibility
-                self.aruco_dict = cv2.aruco.extendDictionary(32, 3)
-            except AttributeError:
-                self.aruco_dict = cv2.aruco.Dictionary_create(32, 3)
+        try:
+            wid = int(cr.json['dict'])
+        except ValueError:
+            if cr.json['dict'] in ARUCO_DICT:
+                wid = ARUCO_DICT[cr.json['dict']]
+            else:
+                wid = 1
+        if wid == 99:     # use special 3x3 dictionary
+            self.aruco_dict = cv2.aruco.extendDictionary(32, 3)
         else:
-            try:    # for 4.8 compatibility
-                self.aruco_dict = cv2.aruco.getPredefinedDictionary(cr.json['dict'])
-            except:
-                self.aruco_dict = cv2.aruco.Dictionary_get(cr.json['dict'])
+            self.aruco_dict = cv2.aruco.getPredefinedDictionary(wid)
         self.mtx = self.dist = None
         self.calibration = cr.json['calibration']
+        self.pose = cr.json['pose']
         if self.calibration:    # load callibration data
             if path.exists(self.calibration):
-                with open(self.calibration) as f:
-                    c = yaml.load(f, Loader=yaml.FullLoader)
+                with open(self.calibration, encoding='ascii') as f:
+                    if self.calibration[-4:].lower() == 'yaml':
+                        c = yaml.load(f, Loader=yaml.FullLoader)
+                    else:
+                        c = json.loads("".join(f.readlines()))
                     self.mtx = np.array(c['camera_matrix'])
                     self.dist = np.array(c['dist_coeff'])
+                    self.cal_w, self.cal_h = c['img_size']
             else:
                 print('Calibration file not found')
                 sys.exit(1)
@@ -163,14 +169,19 @@ class ArucoBase():
             self.aruco_dict = cv2.aruco.extendDictionary(32, 3)
         else:
             self.aruco_dict = cv2.aruco.getPredefinedDictionary(wid)
-        self.mtx = self.dist = None
+        self.mtx = self.dist = self.cal_w = self.cal_h = None
         self.calibration = args.calibration
+        self.pose = args.pose if self.calibration else False
         if self.calibration:    # load callibration data
             if path.exists(self.calibration):
-                with open(self.calibration) as f:
-                    c = yaml.load(f, Loader=yaml.FullLoader)
+                with open(self.calibration, encoding='ascii') as f:
+                    if self.calibration[-5:].lower() == '.yaml':
+                        c = yaml.load(f, Loader=yaml.FullLoader)
+                    else:
+                        c = json.loads("".join(f.readlines()))
                     self.mtx = np.array(c['camera_matrix'])
                     self.dist = np.array(c['dist_coeff'])
+                    self.cal_w, self.cal_h = c['img_size']
             else:
                 print('Calibration file not found')
                 sys.exit(1)
@@ -214,17 +225,15 @@ class ArucoBase():
             :returns: dictionary of position and pose
         """
         if self.calibration:    # undistort image using calibration
-            # TODO NEW https://stackoverflow.com/questions/39432322/what-does-the-getoptimalnewcameramatrix-do-in-opencv getoptimalnewcameramatrix is neccessary if the calibration and the image resolution is different!
-            # TODO check it https://docs.opencv.org/master/dc/dbb/tutorial_py_calibration.html
-            #h, w = frame.shape[:2]
-            #newmtx, roi = cv2.getOptimalNewCameraMatrix(self.mtx, self.dist,
-            #                                            (w, h), ALFA, (w, h))
-            # TODO next line zoom in image!
-            #frame = cv2.undistort(frame, self.mtx, self.dist, None, newmtx)
-            frame = cv2.undistort(frame, self.mtx, self.dist, None)
-            # crop image
-            #frame = frame[roi[1]:roi[1]+roi[3], roi[0]:roi[0]+roi[2]]
-        img = frame.copy()  # copy original (undistorted) image for display
+            h, w = frame.shape[:2]
+            if (self.cal_w, self.cal_h) == (h, w):
+                newmtx = self.mtx
+            else:
+                newmtx, roi = cv2.getOptimalNewCameraMatrix(self.mtx, self.dist,
+                                                            (self.cal_w, self.cal_h), ALFA, (w, h))
+            img = cv2.undistort(frame, self.mtx, self.dist, None, newmtx)
+        else:
+            img = frame.copy()  # copy original (undistorted) if no calibration
         if self.hist:
             if self.lchanel:
                 lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
@@ -257,13 +266,13 @@ class ArucoBase():
                                    hypot(corners[j][0][1, 0] - corners[j][0][2, 0],
                                          corners[j][0][1, 1] - corners[j][0][2, 1]))
                     actCorner = corners[j][0]
-                    if self.calibration:    # estimate pose
+                    if self.pose:    # estimate pose
                         rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners[j:j+1], self.size, self.mtx, self.dist)
                         # https://answers.opencv.org/question/16796/computing-attituderoll-pitch-yaw-from-solvepnp/?answer=52913#post-id-52913
                         r, _ = cv2.Rodrigues(rvec) # convert to rotation matrix
                         # https://www.learnopencv.com/rotation-matrix-to-euler-angles/
                         euler_angles = self.rotationMatrixToEulerAngles(r)
-                    if self.calibration:    # output pose, too
+                    if self.pose:    # output pose, too
                         res.append({'code': ids[j][0], 'east': x, 'north': y,
                                     'width': marker_w, 'height': marker_h,
                                     'euler_angles': euler_angles})
